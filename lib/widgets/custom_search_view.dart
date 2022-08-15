cc2905e176589f2610ae7779feb18763481ec1d9
goodluck wile <goodluckwileonline@gmail.com>
2022-08-15 11:24:26 +0300

    fix some layout on homepage

---
+++ lib/widgets/custom_search_view.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+import '../helpers/responsive.dart';
+import '../themes/colors.dart';
+
+class CustomSearchView extends StatelessWidget {
+  CustomSearchView(
+      {this.shape,
+      this.padding,
+      this.variant,
+      this.fontStyle,
+      this.alignment,
+      this.width,
+      this.margin,
+      this.controller,
+      this.focusNode,
+      this.hintText,
+      this.prefix,
+      this.prefixConstraints,
+      this.suffix,
+      this.suffixConstraints,});
+
+  SearchViewShape? shape;
+
+  SearchViewPadding? padding;
+
+  SearchViewVariant? variant;
+
+  SearchViewFontStyle? fontStyle;
+
+  Alignment? alignment;
+
+  double? width;
+
+  EdgeInsetsGeometry? margin;
+
+  TextEditingController? controller;
+
+  FocusNode? focusNode;
+
+  String? hintText;
+
+  Widget? prefix;
+
+  BoxConstraints? prefixConstraints;
+
+  Widget? suffix;
+
+  BoxConstraints? suffixConstraints;
+
+  @override
+  Widget build(BuildContext context) {
+    return alignment != null
+        ? Align(
+            alignment: alignment ?? Alignment.center,
+            child: _buildSearchViewWidget(),
+          )
+        : _buildSearchViewWidget();
+  }
+
+  _buildSearchViewWidget() {
+    return Container(
+      width: getHorizontalSize(width ?? 0),
+      margin: margin,
+      child: TextFormField(
+        controller: controller,
+        focusNode: focusNode,
+        style: _setFontStyle(),
+        decoration: _buildDecoration(),
+      ),
+    );
+  }
+
+  _buildDecoration() {
+    return InputDecoration(
+      hintText: hintText ?? "",
+      hintStyle: _setFontStyle(),
+      border: _setBorderStyle(),
+      enabledBorder: _setBorderStyle(),
+      focusedBorder: _setBorderStyle(),
+      disabledBorder: _setBorderStyle(),
+      prefixIcon: prefix,
+      prefixIconConstraints: prefixConstraints,
+      suffixIcon: suffix,
+      suffixIconConstraints: suffixConstraints,
+      fillColor: _setFillColor(),
+      filled: _setFilled(),
+      isDense: true,
+      contentPadding: _setPadding(),
+    );
+  }
+
+  _setFontStyle() {
+    switch (fontStyle) {
+      default:
+        return TextStyle(
+          color: ColorConstant.gray500,
+          fontSize: getFontSize(
+            15,
+          ),
+          fontFamily: 'Poppins',
+          fontWeight: FontWeight.w500,
+        );
+    }
+  }
+
+  _setOutlineBorderRadius() {
+    switch (shape) {
+      default:
+        return BorderRadius.circular(
+          getHorizontalSize(
+            14.00,
+          ),
+        );
+    }
+  }
+
+  _setBorderStyle() {
+    switch (variant) {
+      default:
+        return OutlineInputBorder(
+          borderRadius: _setOutlineBorderRadius(),
+          borderSide: BorderSide.none,
+        );
+    }
+  }
+
+  _setFillColor() {
+    switch (variant) {
+      default:
+        return ColorConstant.gray100;
+    }
+  }
+
+  _setFilled() {
+    switch (variant) {
+      case SearchViewVariant.FillGray100:
+        return true;
+      default:
+        return true;
+    }
+  }
+
+  _setPadding() {
+    switch (padding) {
+      default:
+        return getPadding(
+          all: 13,
+        );
+    }
+  }
+}
+
+enum SearchViewShape {
+  RoundedBorder14,
+}
+
+enum SearchViewPadding {
+  PaddingAll13,
+}
+
+enum SearchViewVariant {
+  FillGray100,
+}
+
+enum SearchViewFontStyle {
+  PoppinsMedium15,
+}
